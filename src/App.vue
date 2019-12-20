db61f408506732ffd60fb3af0ab3a70838346015
ROBERTO MILANI <robimilani@gmail.com>
2019-12-20 13:45:44 +0100

    191220 cleaned App component; temporarily disabled automatic fetchUserData on created() HeaderNav

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,6 @@
 <template>
   <v-app>
     <HeaderNav />
-    <!--<v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>SALVO!</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <span class="mr-2">Vuetify</span>
-      <v-btn text href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank">
-        <span class="mr-2">latest release</span>
-      </v-btn>
-      <router-link to="/">Home</router-link>|
-      <router-link to="/game_view/1">Game view 1</router-link>|
-      <router-link to="/leaderboard">Leaderboard</router-link>|
-      <router-link to="/login">Login</router-link>|
-    </v-app-bar>-->
 
     <v-content>
       <router-view />
